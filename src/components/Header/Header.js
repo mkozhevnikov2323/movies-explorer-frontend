4fc8b1573f363ea3b960fc90eea18a708f20a179
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-08-09 22:55:32 +0400

    dev: add jsx to Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../images/logo.svg';
+import Navigation from '../Navigation/Navigation'
 
 export default function Header() {
   return (
     <header>
-      <Link>
-        <img className='logo' src='' alt='Логотип'/>
+      <Link to='/'>
+        <img className='logo' src={logo} alt='Логотип'/>
       </Link>
+      <Navigation />
     </header>
   )
 }
