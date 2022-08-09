05c2a9fa9cde0843804a48b46617ec1c9d6ff714
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-08-09 22:20:55 +0400

    dev: add jsx to Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 export default function Main() {
   return (
     <div>
+      <Header />
 
+      <Footer />
     </div>
   )
 }
