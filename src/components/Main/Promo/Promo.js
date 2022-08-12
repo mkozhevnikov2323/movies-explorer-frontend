8d6bac727420e85d971e78285379cc56e2b83b84
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-08-12 22:50:53 +0400

    dev: add fonts & normalize.css

--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -6,7 +6,7 @@ export default function Promo() {
   return (
     <section className='promo'>
       <img className='promo__image' src={globe} alt='Глобус'/>
-      <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
+      <h1 className='promo__title'>Учебный проект студента факультета <span className='promo__course'>Веб-разработки.</span></h1>
       <p className='promo__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
       <button className='promo__button'>Узнать больше</button>
     </section>
