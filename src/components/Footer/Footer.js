457b53e1223733a3dfbcf2072a92365b9ea2477a
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-08-13 22:21:36 +0400

    dev: add section Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
 
 export default function Footer() {
   return (
-    <div>
-
-    </div>
-  )
+    <footer className="footer">
+      <p className="footer__text">
+        Учебный проект Яндекс.Практикум х BeatFilm.
+      </p>
+      <div className="footer__info">
+        <p className="footer_copyright">© 2022</p>
+        <ul className="footer__links">
+          <li className="footer__link-item">
+            <a
+              href="https://praktikum.yandex.ru/"
+              className="footer__link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Яндекс.Практикум
+            </a>
+          </li>
+          <li className="footer__link-item">
+            <a
+              href="https://github.com/"
+              className="footer__link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './AboutMe.css';
-import studentPhoto from '../../../images/student.jpg';
+import studentPhoto from '../../../images/mkozhevnikov.jpg';
 
 export default function AboutMe() {
   return (
@@ -9,7 +9,7 @@ export default function AboutMe() {
       <img alt='Фото' src={studentPhoto} className='about-me__photo'/>
       <h3 className='about-me__subtitle'>Максим</h3>
       <h4 className='about-me__description'>Фронтенд-разработчик, 35 лет</h4>
-      <p className='about-me__text'>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+      <p className='about-me__text'>Я родился и живу в Ижевске, закончил факультет менеджмента и маркетинга ИжГТУ. Люблю слушать музыку, а ещё увлекаюсь автопутешествиями. Недавно начал кодить. Работал в автобизнесе и банковском секторе с 2010 года. После того, как прошёл курс по веб-разработке, устроился в компанию IconiCompany.</p>
       <a href='https://github.com/mkozhevnikov2323' target='_blank' rel="noreferrer" className='about-me__link'>Github</a>
     </section>
   )
