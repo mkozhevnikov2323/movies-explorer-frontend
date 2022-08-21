181ceeb1e5ab5349b6f93c5276b6fe5c0702c866
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-08-21 16:26:53 +0400

    dev: add component SavedMovies

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './SavedMovies.css';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import SearchForm from '../Movies/SearchForm/SearchForm';
+import MoviesCardList from '../Movies/MoviesCardList/MoviesCardList';
 
 export default function SavedMovies() {
   return (
-    <div>
-
-    </div>
+    <>
+      <Header />
+      <main className='savedMovies'>
+        <SearchForm />
+        <MoviesCardList />
+      </main>
+      <Footer />
+    </>
   )
 }
