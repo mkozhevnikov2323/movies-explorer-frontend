10257e15292196e9dda8f1853b54a3ae98ba6d71
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-08-25 22:26:41 +0400

    dev: refactor links on Portfolio

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -8,43 +8,37 @@ export default function Portfolio() {
       <h2 className="portfolio__title">Портфолио</h2>
       <ul className="portfolio__list">
         <li className="portfolio__item">
-          <p className="portfolio__example">
+          <a
+            className="portfolio__link"
+            href="https://mkozhevnikov2323.github.io/how-to-learn/index.html"
+            target='_blank'
+            rel="noreferrer"
+          >
             Статичный сайт
-            <a
-              className="portfolio__link"
-              href="https://mkozhevnikov2323.github.io/how-to-learn/index.html"
-              target='_blank'
-              rel="noreferrer"
-            >
-              <img className="portfolio__image-link" alt="Стрелка" src={link} />
-            </a>
-          </p>
+            <img className="portfolio__image-link" alt="Стрелка" src={link} />
+          </a>
         </li>
         <li className="portfolio__item">
-          <p className="portfolio__example">
+          <a
+            className="portfolio__link"
+            href="https://mkozhevnikov2323.github.io/russian-travel/index.html"
+            target='_blank'
+            rel="noreferrer"
+          >
             Адаптивный сайт
-            <a
-              className="portfolio__link"
-              href="https://mkozhevnikov2323.github.io/russian-travel/index.html"
-              target='_blank'
-              rel="noreferrer"
-            >
-              <img className="portfolio__image-link" alt="Стрелка" src={link} />
-            </a>
-          </p>
+            <img className="portfolio__image-link" alt="Стрелка" src={link} />
+          </a>
         </li>
         <li className="portfolio__item">
-          <p className="portfolio__example">
+          <a
+            className="portfolio__link"
+            href="https://mkozhevnikov2323.github.io/react-mesto-auth/index.html"
+            target='_blank'
+            rel="noreferrer"
+          >
             Одностраничное приложение
-            <a
-              className="portfolio__link"
-              href="https://mkozhevnikov2323.github.io/react-mesto-auth/index.html"
-              target='_blank'
-              rel="noreferrer"
-            >
-              <img className="portfolio__image-link" alt="Стрелка" src={link} />
-            </a>
-          </p>
+            <img className="portfolio__image-link" alt="Стрелка" src={link} />
+          </a>
         </li>
       </ul>
     </section>
