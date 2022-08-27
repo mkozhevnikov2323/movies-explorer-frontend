d48e68747ec9bfca5516765bf71a3fcc2368de72
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-08-28 00:26:24 +0400

    dev: add component PageNotFound

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,7 @@ import SavedMovies from '../SavedMovies/SavedMovies';
 import Profile from '../Profile/Profile';
 import Login from '../Login/Login';
 import Register from '../Register/Register';
+import PageNotFound from '../PageNotFound/PageNotFound';
 import './App.css';
 
 export default function App() {
@@ -18,6 +19,7 @@ export default function App() {
         <Route path='/profile' element={<Profile />}/>
         <Route path='/signin' element={<Login />}/>
         <Route path='/signup' element={<Register />}/>
+        <Route path='*' element={<PageNotFound />}/>
       </Routes>
     </div>
   )
